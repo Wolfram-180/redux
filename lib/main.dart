b65d05c301e6cf4655be81c4b16625a3f4c1dc03
Wolfram-180 <sergei.ulvis@gmail.com>
2024-02-14 17:00:57 +0300

    itemsReducer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:redux/redux.dart';
 
 void main() {
   runApp(
@@ -25,3 +26,80 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+
+enum ItemFilter {
+  all,
+  longTexts,
+  shortTexts,
+}
+
+@immutable
+class State {
+  final Iterable<String> items;
+  final ItemFilter filter;
+
+  const State({
+    required this.items,
+    required this.filter,
+  });
+
+  Iterable<String> get filteredItems {
+    switch (filter) {
+      case ItemFilter.all:
+        return items;
+      case ItemFilter.longTexts:
+        return items.where((item) => item.length >= 10);
+      case ItemFilter.shortTexts:
+        return items.where((item) => item.length <= 3);
+    }
+  }
+}
+
+@immutable
+abstract class Action {
+  const Action();
+}
+
+@immutable
+class ChangeFilterTypeAction extends Action {
+  final ItemFilter filter;
+  const ChangeFilterTypeAction(this.filter);
+}
+
+@immutable
+abstract class ItemAction extends Action {
+  final String item;
+  const ItemAction({required this.item});
+}
+
+@immutable
+class AddItemAction extends ItemAction {
+  const AddItemAction({required String item}) : super(item: item);
+}
+
+@immutable
+class RemoveItemAction extends ItemAction {
+  const RemoveItemAction({required String item}) : super(item: item);
+}
+
+extension AddRemoveItems<T> on Iterable<T> {
+  Iterable<T> operator +(T other) => followedBy([other]);
+  Iterable<T> operator -(T other) => where((elements) => elements != other);
+}
+
+Iterable<String> addItemReducer(
+  Iterable<String> previousItems,
+  AddItemAction action,
+) =>
+    previousItems + action.item;
+
+Iterable<String> removeItemReducer(
+  Iterable<String> previousItems,
+  RemoveItemAction action,
+) =>
+    previousItems - action.item;
+
+Reducer<Iterable<String>> itemsReducer = combineReducers<Iterable<String>>([
+  TypedReducer<Iterable<String>, AddItemAction>(addItemReducer),
+  TypedReducer<Iterable<String>, RemoveItemAction>(removeItemReducer),
+]);
